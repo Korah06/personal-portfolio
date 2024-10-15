3d87b8230918469b52f60b6ce769adc1acc7fbbe
Mario Espasa <73080627+Korah06@users.noreply.github.com>
2024-10-15 22:35:50 +0200

    Added color theme

---
+++ lib/ui/theme/color_schema.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+const ColorScheme themeColorSchema = ColorScheme(
+  primary: Color(0xFF9290C3), // Used for primary elements
+  primaryContainer: Color(0xFF1B1A55), // Used for primary container elements
+  secondary: Color(0xFF535C91), // Used for secondary elements
+  secondaryContainer: Color(0xFF9290C3), // Used for secondary container elements
+  surface: Color(0xFF535C91), // Used for surfaces like cards
+  background: Color(0xFF070F2B), // Used for background color
+  error: Color(0xFFE76F51), // Used for error elements
+  onPrimary: Colors.white, // Used for text/icons on primary elements
+  onSecondary: Colors.white, // Used for text/icons on secondary elements
+  onSurface: Colors.white, // Used for text/icons on surfaces
+  onBackground: Colors.white, // Used for text/icons on background
+  onError: Colors.white, // Used for text/icons on error elements
+  brightness: Brightness.light, // Defines the overall brightness of the theme
+);
